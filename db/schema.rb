5647c40c8ce8951290dd76f8c6570226c792f764
Dmytro Korlotian <dima@korlotian.pl>
2016-07-29 12:28:47 +0200

    create model restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729082536) do
+ActiveRecord::Schema.define(version: 20160729102720) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "telephone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :restaurant do
+    name "MyString"
+    telephone "MyString"
+  end
+end
