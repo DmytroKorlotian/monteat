4ed006050c1b10a774512c0141de15b6413b017e
Dmytro Korlotian <dima@korlotian.pl>
2016-08-02 23:20:41 +0200

    one user can have only one order per day

--- app/controllers/meal_orders_controller.rb
+++ app/controllers/meal_orders_controller.rb
@@ -6,4 +6,29 @@ class MealOrdersController < ApplicationController
 
   def index
   end
+
+  def create
+    @meal_order = MealOrder.new(meal_order_params)
+    @meal_order.user_id = current_user.id
+    @meal_order.order_id = todays_order.id
+
+    respond_to do |format|
+      if !MealOrder.today.by_user(current_user).present? && @meal_order.save
+        format.json { head :ok }
+      else
+        format.json { render text: '{"error": "Cheater!!! You have already ordered your meal!"}' }
+      end
+    end
+  end
+
+  private
+
+  def meal_order_params
+    params.require(:meal_order).permit(:meal_id)
+  end
+
+  def todays_order
+    order.create! unless Order.today.present?
+    Order.today.first
+  end
 end
