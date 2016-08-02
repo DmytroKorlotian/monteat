9ec781185710d61e01c8b2a06ee57d8dbf9f43cf
Dmytro Korlotian <dima@korlotian.pl>
2016-08-02 23:22:33 +0200

    add scopes

--- app/models/meal_order.rb
+++ app/models/meal_order.rb
@@ -1,3 +1,6 @@
 class MealOrder < ApplicationRecord
   belongs_to :order
+
+  scope :today, -> { where('DATE(created_at) = ?', Date.today) }
+  scope :by_user, -> (user) { where('user_id', user.id) }
 end
