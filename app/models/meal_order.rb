b750e0dc3bce07dfdc6f83bfc3e1a2df3af8573f
Dmytro Korlotian <dima@korlotian.pl>
2016-08-05 00:40:36 +0200

    changing time to utc

--- app/models/meal_order.rb
+++ app/models/meal_order.rb
@@ -1,8 +1,8 @@
 class MealOrder < ApplicationRecord
   belongs_to :order
 
-  scope :today, -> { where('DATE(created_at) = ?', Date.today) }
-  scope :not_today, -> { where.not('DATE(created_at) = ?', Date.today) }
+  scope :today, -> { where('DATE(created_at) = ?', Time.now.utc.to_date) }
+  scope :not_today, -> { where.not('DATE(created_at) = ?', Time.now.utc.to_date) }
   scope :by_user, -> (user) { where('user_id', user.id) }
 
   def meal
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   has_many :meal_orders
 
-  scope :today, lambda { where('DATE(created_at) = ?', Date.today) }
+  scope :today, lambda { where('DATE(created_at) = ?', Time.now.utc.to_date) }
 end
