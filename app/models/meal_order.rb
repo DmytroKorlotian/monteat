5f7fd0cc05dceb43f0b70be2dd96caa6b248b605
Dmytro Korlotian <dima@korlotian.pl>
2016-08-04 23:44:49 +0200

    add not today order to display archived orders

--- app/models/meal_order.rb
+++ app/models/meal_order.rb
@@ -2,5 +2,14 @@ class MealOrder < ApplicationRecord
   belongs_to :order
 
   scope :today, -> { where('DATE(created_at) = ?', Date.today) }
+  scope :not_today, -> { where.not('DATE(created_at) = ?', Date.today) }
   scope :by_user, -> (user) { where('user_id', user.id) }
+
+  def meal
+    Meal.find(meal_id)
+  end
+
+  def order
+    Order.find(order_id)
+  end
 end
