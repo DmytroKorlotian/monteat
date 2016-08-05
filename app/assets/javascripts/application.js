e2e61c529375f8cd864c6f009396a9dd91cafa52
Dmytro Korlotian <dima@korlotian.pl>
2016-08-05 14:08:11 +0200

    change applicationjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,27 +22,3 @@
 //= require_tree ./monteat/modules
 //= require_tree ./monteat/services
 //= require_tree ./monteat/controllers
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require angular
-//= require angular-ui-select
-//= require angular-sanitize
-//= require angular-rails-templates
-//= require ./monteat/app
-//= require_tree .
-//= require_tree ./monteat/modules
-//= require_tree ./monteat/services
-//= require_tree ./monteat/controllers
